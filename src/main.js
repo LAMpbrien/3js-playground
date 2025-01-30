db77f6251281e264a3e516fc863d6c61b1356021
LAMpbrien <patrick@loveandmoney.agency>
2025-01-30 22:09:25 +1100

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,76 @@
+import './style.css'
+
+import { animate } from 'motion'
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const orchidTag = document.querySelector("section.orchid")
+
+//loading animations
+animate (".left-hand-side .version-history", {
+  y: [-40,0],
+  opacity: [0, 1]
+}, { duration: 0.4, delay: 0.25})
+
+animate (".install-area img", {
+  y: [-50,0],
+  opacity: [0, 1]
+}, { duration: 0.5, delay: 0.25})
+
+animate (".install-area .big-boi", {
+  y: [-60,0],
+  opacity: [0, 1]
+}, { duration: 0.6, delay: 0.3})
+
+animate (".install-area .latest-release", {
+  y: [-70,0],
+  opacity: [0, 1]
+}, { duration: 0.7, delay: 0.6})
+
+animate (".left-hand-side #heading-copy", {
+  y: [-100,0],
+  opacity: [0, 1]
+}, { duration: 0.8, delay: 0.6})
+
+
+//3js
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor(0x000000, 0)
+orchidTag.appendChild( renderer.domElement );
+
+
+//lighting
+const ambience = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( ambience );
+
+const keyLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+keyLight.position.set(-1,1,3)
+scene.add( keyLight );
+
+const fillLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+fillLight.position.set(1,1,3)
+scene.add( fillLight );
+
+const backLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+backLight.position.set(-1,3,-1)
+scene.add( backLight );
+
+// shape+material
+
+const geometry = new THREE.TorusKnotGeometry( 1, 0.25, 100, 16 ); 
+const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } ); 
+const torusKnot = new THREE.Mesh( geometry, material ); scene.add( torusKnot );
+
+camera.position.z = 5;
+
+function render() {
+  torusKnot.rotation.x += 0.01;
+  torusKnot.rotation.y += 0.01;
+	renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( render );
