6e7687447ee8e42b1ce7b5c3dc11334fd86a8fe9
LAMpbrien <patrick@loveandmoney.agency>
2025-01-31 15:26:07 +1100

    Add 3d model

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import './style.css'
 
 import { animate } from 'motion'
-import * as THREE from 'three';
+// import * as THREE from 'three';
 
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import {
+  AmbientLight,
+  DirectionalLight,
+  Group,
+  Mesh,
+  MeshLambertMaterial,
+  PerspectiveCamera,
+  Scene,
+  TorusKnotGeometry,
+  WebGLRenderer,
+} from 'three'
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 const orchidTag = document.querySelector("section.orchid")
 
@@ -35,28 +48,28 @@ animate (".left-hand-side #heading-copy", {
 
 
 //3js
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new Scene();
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor(0x000000, 0)
 orchidTag.appendChild( renderer.domElement );
 
 
 //lighting
-const ambience = new THREE.AmbientLight( 0x404040 ); // soft white light
+const ambience = new AmbientLight( 0x404040 ); // soft white light
 camera.add( ambience );
 
-const keyLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+const keyLight = new DirectionalLight( 0xffffff, 0.5 );
 keyLight.position.set(-1,1,3)
 camera.add( keyLight );
 
-const fillLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+const fillLight = new DirectionalLight( 0xffffff, 0.5 );
 fillLight.position.set(1,1,3)
 camera.add( fillLight );
 
-const backLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+const backLight = new DirectionalLight( 0xffffff, 0.5 );
 backLight.position.set(-1,3,-1)
 camera.add( backLight );
 
@@ -64,10 +77,29 @@ scene.add(camera)
 
 // shape+material
 
-const geometry = new THREE.TorusKnotGeometry( 1, 0.25, 100, 16 )
-const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } )
-const torusKnot = new THREE.Mesh( geometry, material )
-scene.add( torusKnot )
+// const geometry = new TorusKnotGeometry( 1, 0.25, 100, 16 )
+// const material = new MeshLambertMaterial( { color: 0x00ff00 } )
+// const torusKnot = new Mesh( geometry, material )
+
+// scene.add(torusKnot)
+
+//OBJECT IMPORT
+const gltfLoader = new GLTFLoader()
+
+gltfLoader.load("orchid-3d-model.glb", (gltf) => {
+  scene.add(gltf.scene)
+})
+
+// TO DO: Group to animate on load
+// const loadGroup = new Group()
+// loadGroup.position.y = -10
+// loadGroup.add(torusKnot)
+
+// scene.add(loadGroup)
+
+// animate((t) => {
+//   loadGroup.position.y = -10 + 10 * t
+// }, {duration: 2, delay: 1})
 
 
 // Controls
@@ -79,7 +111,7 @@ controls.autoRotateSpeed = 2
 controls.update()
 
 
-camera.position.z = 5;
+camera.position.z = 0.27;
 
 function render() {
   controls.update()
